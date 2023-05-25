cb564dc62a88ca3e34e6c4c1ee783a96a078e75d
Crowdbotics <team@crowdbotics.com>
2023-05-25 23:43:19 +0000

    Add Copy of Mob_6 (1:2) 1

---
+++ screens/CopyofMob_6(1:2)1/index.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const CopyofMob_6(1:2)1 = () => {
+  return <View style={_styles.CopyofMob_6(1:2)1}>
+      </View>;
+};
+
+export default CopyofMob_6(1:2)1;
+
+const _styles = StyleSheet.create({
+  CopyofMob_6(1:2)1: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
